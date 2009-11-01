465300884586e7dc8dbb64bf1781c1897d1b9fb8
Lorenz Diener <halcyon@lain-chan.(none)>
2009-11-01 23:32:45 +0100

    Okay, maybe I should add a license, I guess.

--- pixivrss.pl
+++ pixivrss.pl
@@ -1,5 +1,23 @@
 #!/usr/bin/perl
 
+# pixivrss.pl - Pixiv "bookmarks" RSS feed generator.                
+
+# (c) 2009 L. Diener, licensed under the WTFPL, see below.
+
+#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                     Version 2, December 2004           
+#                                                        
+#  Copyright (C) 2004 Sam Hocevar                        
+#   14 rue de Plaisance, 75014 Paris, France             
+#  Everyone is permitted to copy and distribute verbatim or modified
+#  copies of this license document, and changing it is allowed as long
+#  as the name is changed.                                            
+#                                                                     
+#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE             
+#    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION  
+#                                                                     
+#   0. You just DO WHAT THE FUCK YOU WANT TO.                         
+
 use strict;
 use warnings;
 
